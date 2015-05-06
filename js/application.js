581976658efe50b8dc6248aa8c16d1c1ebc32d6e
Freetown5 <iconteh21@yahoo.com>
2015-05-06 08:58:08 -0400

    my atttempt at stocktrader

--- js/application.js
+++ js/application.js
@@ -1,1 +1,3 @@
 // instantiate a StockView
+var stock = new StockView;
+// var stockView = new StockView(".Symbol ", Stock )
--- js/views/stock.js
+++ js/views/stock.js
@@ -1,1 +1,21 @@
 // define your StockView here
+
+// - `StockView`
+//   - `model`: points to an instance of a Stock model.
+//   - `render`: updates the HTML on the page to reflect the info of the associated stock model. The info is the price, companyName and totalValue.
+//   - 'lookupSymbol': when the user enters a symbol, this should instantiate a new instance of a stock model and associate it with the `model` attribute of the view. It should then re-`render` the view.
+//   - updateShares: when the user enters a number of shares, it should update the existing associated stock model, and then re-render the view
+
+var StockView = function(model){
+  this.model = model;
+  this.render();
+}
+
+StockView.prototype = {
+    render:function(stuff){
+      model.innerHTML(stuff);
+      console.log(this);
+  }
+}
+
+var GOOGL = new StockView("google")
