280dd76649533de02aa6c4fa3ab1a00ac5221eff
Brad Foster <btf5048@gmail.com>
2015-05-06 19:08:20 -0400

    ajax api call

--- js/models/stock.js
+++ js/models/stock.js
@@ -4,9 +4,19 @@ function Stock(symbol) {
   this.numShares = 0;
 
   // your code to search the API and return a Stock object here
+    $.ajax({
+      url: "http://dev.markitondemand.com/api/v2/quote/jsonp?symbol=" + symbol,
+      method: "get",
+      dataType: "jsonp",
+      context: this
+    }).done(function(data){
+      $("#stock-name").append(data["Name"]);
+      $("#stock-price").append(data["LastPrice"]);
+    });
 
 }
 
 Stock.prototype = {
   totalValue: function() { return this.price * this.numShares; }
+  // $("#total-value").append(totalValue);
 }
