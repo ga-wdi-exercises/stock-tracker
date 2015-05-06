0595d7be24254d89ba501f46e1715b0facbb96e6
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-06 18:36:16 -0400

    achieved functionality in the console

--- js/models/stock.js
+++ js/models/stock.js
@@ -2,11 +2,37 @@
 function Stock(symbol) {
   this.symbol = symbol;
   this.numShares = 0;
-
-  // your code to search the API and return a Stock object here
-
+  this.data = this.lookupStock();
 }
-
+// Card.prototype = {
+//   createInRails: function(){
+//     $.ajax({
+//       type: "POST",
+//       dataType: "json",
+//       data: {card:{description: this.description, completed: this.completed}},
+//       url: "http://localhost:3000/cards"
+//     }).done(function(){
+//         trilloModel.fetchCards();
+//     }).fail(function(){
+//       console.log("saving to rails API failed")
+//     })
 Stock.prototype = {
-  totalValue: function() { return this.price * this.numShares; }
+  totalValue: function() { return this.price * this.numShares; },
+  lookupStock: function(){
+    $.ajax({
+      url:"http://dev.markitondemand.com/api/v2/quote/jsonp?symbol=" + this.symbol,
+      type: "GET",
+      dataType: "jsonp"
+      //data:
+    }).done(function(response){
+      console.log(response)
+      console.log(response.Name)
+      console.log(response.LastPrice)
+      // $("#stock-name").text(this.model.companyName);
+      // $("#stock-price").text(this.model.price);
+      // $("#total-value").text(this.model.totalValue());
+    }).fail(function(){
+      console.log("ajax request unsuccessful")
+    })
+  }
 }
