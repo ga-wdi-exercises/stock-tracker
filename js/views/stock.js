1969a17947fd94c7519cca838cdf958ccacfc14c
Toby <toby.schaeffer@gmail.com>
2015-05-06 20:02:29 -0400

    completed initial assignment

--- js/views/stock.js
+++ js/views/stock.js
@@ -2,14 +2,17 @@
 function StockView() {
   $('#update-symbol').on('click', this.lookupSymbol.bind(this));
   $('#update-shares').on('click', this.updateShares.bind(this));
-}
+};
 
 StockView.prototype = {
   lookupSymbol: function() {
     var symbol = $("#symbol-input").val();
     this.model = new Stock(symbol);
-    this.render();
-    $('#cha-ching')[0].play();
+    this.model.refresh().then(function(){
+      this.render();
+      $('#cha-ching')[0].play();
+    }.bind(this));
+    console.log(symbol);
   },
 
   updateShares: function() {
