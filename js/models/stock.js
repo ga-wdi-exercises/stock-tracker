f65aab2e6587c5650dd8194d700b847ea7ca37fa
Nabil Hashmi <nabil2011@gmail.com>
2015-05-06 21:58:14 -0400

    Render stock information, but with two clicks

--- js/models/stock.js
+++ js/models/stock.js
@@ -2,11 +2,24 @@
 function Stock(symbol) {
   this.symbol = symbol;
   this.numShares = 0;
-
-  // your code to search the API and return a Stock object here
-
+  $.ajax({
+      url: "http://dev.markitondemand.com/api/v2/quote/jsonp?symbol=" + this.symbol,
+      type: "get",
+      dataType: "jsonp",
+      context: this
+    }).done(function(response){
+      document.querySelector("#stock-price").innerHTML = response.LastPrice;
+      this.price = response.LastPrice;
+      document.querySelector("#stock-name").innerHTML = response.Name;
+      this.companyName = response.Name;
+    })
 }
 
 Stock.prototype = {
-  totalValue: function() { return this.price * this.numShares; }
+  getPrice: function() { return this.price; },
+  getName: function() {return this.companyName; },
+  totalValue: function() { 
+    console.log(this);
+    return this.price * this.numShares; }
+
 }
