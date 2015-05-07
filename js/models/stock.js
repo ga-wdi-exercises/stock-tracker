57e91679110c0d9aff57e630b342e4d67a08ee5c
SmallNic <nas231@nyu.edu>
2015-05-06 20:27:32 -0400

    AJAX API implemented

--- js/models/stock.js
+++ js/models/stock.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-
   /* - `Stock` Model
     - Your constructor function should work like `new Stock(symbol)` where symbol is something like 'AAPL'. It should use the 'symbol' property to lookup the correct stock from the 'data' provided.
     - `price`: the price of the stock from the data provided
@@ -7,27 +5,48 @@ $(document).ready(function(){
     - `numShares`: defaults to 0, but the value of this property may be updated by the view.
     - `totalValue`: returns the price * the number of shares.*/
 
-  var data = [{"Status":"SUCCESS","Name":"Apple  Inc","Symbol":"AAPL", "LastPrice": 128.7, "Change":0,"ChangePercent":0,"Timestamp":"Mon May 4 15:59:00 UTC-04:00 2015",  "MSDate":42128.6659722222,"MarketCap":741444561000,"Volume":3318886, "ChangeYTD":110.38, "ChangePercentYTD":16.5972096394274,"High":130.55,"Low":128.27,"Open":129.58},{"Status":"SUCCESS","Name":"Google Inc","Symbol":"GOOGL","LastPrice":552.78,"Change":1.62,"ChangePercent":0.293925538863489,"Timestamp":"Mon May 4 15:59:00 UTC-04:00 2015","MSDate":42128.6659722222,"MarketCap":188341543260,"Volume":74680,"ChangeYTD":530.66,"ChangePercentYTD":4.16839407530246,"High":556.61,"Low":547.6,"Open":550.1},{"Status":"SUCCESS","Name":"Microsoft Corp","Symbol":"MSFT","LastPrice":48.24,"Change":0,"ChangePercent":0,"Timestamp":"Mon May 4 15:59:00 UTC-04:00 2015","MSDate":42128.6659722222,"MarketCap":390241098000,"Volume":2875552,"ChangeYTD":46.45,"ChangePercentYTD":3.85360602798708,"High":48.86,"Low":48.18,"Open":48.41},{"Status":"SUCCESS","Name":"3M Co","Symbol":"MMM","LastPrice":159.05,"Change":0.0300000000000011,"ChangePercent":0.0188655515029563,"Timestamp":"Mon May 4 15:59:00 UTC-04:00 2015","MSDate":42128.6659722222,"MarketCap":100891936050,"Volume":153090,"ChangeYTD":164.32,"ChangePercentYTD":-3.20715676728334,"High":159.85,"Low":158.12,"Open":158.12},{"Status":"SUCCESS","Name":"Twitter Inc","Symbol":"TWTR","LastPrice":37.89,"Change":0.00999999999999801,"ChangePercent":0.0263991552270275,"Timestamp":"Mon May 4 15:59:00 UTC-04:00 2015","MSDate":42128.6659722222,"MarketCap":24794496090,"Volume":1394555,"ChangeYTD":35.87,"ChangePercentYTD":5.63144689155284,"High":38.96,"Low":37.56,"Open":38.7},{"Status":"SUCCESS","Name":"Facebook, Inc.","Symbol":"FB","LastPrice":78.81,"Change":-0.179999999999993,"ChangePercent":-0.227876946448908,"Timestamp":"Mon May 4 15:59:00 UTC-04:00 2015","MSDate":42128.6659722222,"MarketCap":221317079160,"Volume":839079,"ChangeYTD":78.02,"ChangePercentYTD":1.01256088182518,"High":79.69,"Low":78.64,"Open":79.28}];
-
   //Constructor function
-  var Stock = function( symbolName ){
-    this.symbol = symbolName
-    for (var i = 0; i < data.length; i ++){
-      console.log(data[i]["Symbol"])
-      if(data[i]["Symbol"] === symbolName){
-        this.price = data[i]["LastPrice"]
-        this.companyName = data[i]["Name"]
-        this.numShares = 0
-        this.totalValue = this.price * this.numShares
-      }
-    }
+  var Stock = function( ){
+    this.companyName = "Company Name"
+    this.numShares = "Number of Shares"
+    this.price = "Price"
+    this.symbol = "Symbol"
+    this.totalValue = "Total Value"
   }
 
   Stock.prototype = {
+    getAjaxResponse: function( symbolName ){
+      console.log("this AJAX:", this)
+      console.log("symbolName:",symbolName)
+      var lookupURL = "http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=" + symbolName
+      $.ajax({
+        url: lookupURL,
+        type: "GET",
+        context: this,
+        dataType: "jsonp"
+      }).done(function( response ){
+        console.log("this DONE:", this)
+        this.saveData(response);
+        console.log("this DONE AFTER:", this)
+        stockView.render()
+      }).fail(function(){
+        console.log("AJAX request was not successful")
+      }).always(function(){
+        console.log("this always happens!")
+      })
+    },
     updateShares: function( update ){
       this.numShares = update
       this.totalValue = this.price * this.numShares
+    },
+    saveData: function( response ){
+      console.log("this:", this)
+      this.symbol = response.Symbol
+      this.price = response.LastPrice
+      this.companyName = response.Name
+      this.numShares = 0
+      this.totalValue = this.price * this.numShares
+      console.log("this:", this)
+
     }
   }
-
-})
--- js/views/stock.js
+++ js/views/stock.js
@@ -4,31 +4,31 @@
 // //   - `render`: updates the HTML on the page to reflect the info of the associated stock model. The info is the price, companyName and totalValue.
 // //   - 'lookupSymbol': when the user enters a symbol, this should instantiate a new instance of a stock model and associate it with the `model` attribute of the view. It should then re-`render` the view.
 // //   - updateShares: when the user enters a number of shares, it should update the existing associated stock model, and then re-render the view
-$(document).ready(function(){
-  var updateForm = $("#updateShares")
-  var updateField = $("#update")
-  var updateButton = $("#updateButton")
-
-  updateButton.on("click", function(){
-    console.log(lookupField.val())
-    console.log(updateField.val())
-    var stock = new Stock( lookupField.val() )
-    stock.updateShares(updateField.val())
-    var stockView = new StockView( stock )
-    stockView.render()
-  })
-
-  var symbol = $("#symbol")
-  var price = $("#price")
-  var compName = $("#compName")
-  var value = $("#value")
 
   var StockView = function( stockModel ){
     this.stockModel = stockModel
+    this.render()
+
+    var lookupForm = $("#lookupForm")
+    var lookupField = $("#lookup")
+    var lookupButton = $("#lookupButton")
+
+    lookupButton.on("click", function( event ){
+      event.preventDefault()
+      var symbol = lookupField.val()
+      console.log("stockView.stockModel BEFORE AJAX: ",stockView.stockModel)
+
+      stockView.stockModel.getAjaxResponse(symbol)
+      console.log("stockView.stockModel AFTER AJAX: ",stockView.stockModel)
+      stockView.render()
+    })
+
+
   }
 
   StockView.prototype = {
     render: function(){
+      console.log("render")
       console.log(this.stockModel.symbol)
       console.log(this.stockModel.price)
       console.log(this.stockModel.companyName)
@@ -42,4 +42,16 @@ $(document).ready(function(){
     }
   }
 
-})
+
+  var updateForm = $("#updateShares")
+  var updateField = $("#update")
+  var updateButton = $("#updateButton")
+
+  updateButton.on("click", function(){
+    console.log(lookupField.val())
+    console.log(updateField.val())
+    var stock = new Stock( lookupField.val() )
+    stock.updateShares(updateField.val())
+    var stockView = new StockView( stock )
+    stockView.render()
+  })
